4ee88d94ace01e21af26f7b57ad4c767f0d63b83
Abavisla <sivajana.bala@gmail.com>
2021-01-27 04:29:18 +0100

    error fix

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();// app configuration
 
 
-app.set('port', (process.env.PORT || 3000));// setup our express applicationapp.use(morgan('dev')); // log every request to the console.
+app.set('port', (process.env.PORT || 3000));// setup our express application
+pp.use(morgan('dev')); // log every request to the console.
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json()); // app routes
 
-require('./routes/webhook_verify')(app);// warming up the engines !! setta !! go !!!.app.listen(app.get('port'), function() {
-  const url = 'http://localhost:' + app.set('port');
+require('./routes/webhook_verify')(app);// warming up the engines !! setta !! go !!!.
+
+app.listen(app.get('port'), function() {
+const url = 'http://localhost:' + app.set('port');
   console.log('Application running on port: ', app.get('port'));});
