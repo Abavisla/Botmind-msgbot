cae746dbd0d55c85c3812a324072dd942d8a52a9
Abavisla <sivajana.bala@gmail.com>
2021-01-27 04:58:32 +0100

    reboot

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,9 +1,8 @@
 const processPostback = require('../processes/postback');
 const processMessage = require('../processes/messages');
 
-
 module.exports = function(app, chalk){
-  app.get('/webhook/', function(req, res) {
+  app.get('/webhook', function(req, res) {
     if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
        console.log('webhook verified');
        res.status(200).send(req.query['hub.challenge']);
@@ -13,12 +12,12 @@ module.exports = function(app, chalk){
      }
   });
   
-  app.post('/webhook/', function(req, res) {
+  app.post('/webhook', function(req, res) {
     //checking for page subscription.
     if (req.body.object === 'page'){
        
        /* Iterate over each entry, there can be multiple entries 
-       if callbacks are batched. */       
+       if callbacks are batched. */
        req.body.entry.forEach(function(entry) {
        // Iterate over each messaging event
           entry.messaging.forEach(function(event) {
