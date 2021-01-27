3a3728df29d4c0a028d5c1812a2fddc501f6a4f4
Abavisla <sivajana.bala@gmail.com>
2021-01-27 18:27:02 +0100

    123132

--- server.js
+++ server.js
@@ -1,131 +1,196 @@
-'use strict'
+'use strict';
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    request = require('request'),
+    config = require('config'),
+    images = require('./pics');
 
-const express = require('express')
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app configuration
-app.set('port', (process.env.PORT || 3000));
+let users = {};
 
+app.listen(8080, () => console.log('Example app listening on port 8989!'));
 
-// setup our express application
-app.use(morgan('dev')); // log every request to the console.
-app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json()); 
-
-
-// app routes
-app.get('/', function (req, res) {
-	res.send('Hello world!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
 
-app.get('/webhook', function (req, res) {
-	if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-		res.send(req.query['hub.challenge']);
-	}
-	res.send('Wrong token!');
-});
-
-
+// Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-  let body = req.body;
+    let body = req.body;
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
 
-  if (body.object === 'page') {
-      body.entry.forEach(function(entry) {
-          let webhook_event = entry.messaging[0];
-          console.log(webhook_event);
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
 
-          let sender_psid = webhook_event.sender.id;
-          console.log('Sender PSID: ' + sender_psid);
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-          if (webhook_event.message) {
-            handleMessage(sender_psid, webhook_event.message);
-        } else if (webhook_event.postback) {
-            handlePostback(sender_psid, webhook_event.postback);
-        }else if(webhook_event.attachedfile){
-          }
-      });
-      res.status(200).send('EVENT_RECEIVED');
-  } else {
-      res.sendStatus(404);
-  }
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 
 });
 
-const handleMessage = (sender_psid, received_message) => {
-  let response;
-  if (received_message.text) {
-    callSendAPI(sender_psid,received_message.text);
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    console.log('webhook verified');
+    res.status(200).send(req.query['hub.challenge']);
+ } else {
+     console.error('verification failed. Token mismatch.');
+     res.sendStatus(403);
   }
+});
+
+
+function getImage(type, sender_id){
+    // create user if doesn't exist
+    if(users[sender_id] === undefined){
+        users = Object.assign({
+            [sender_id] : {
+                'cats_count' : 0,
+                'dogs_count' : 0
+            }
+        }, users);
+    }
+
+    let count = images[type].length, // total available images by type
+        user = users[sender_id], // // user requesting image
+        user_type_count = user[type+'_count'];
+
+
+    // update user before returning image
+    let updated_user = {
+        [sender_id] : Object.assign(user, {
+            [type+'_count'] : count === user_type_count + 1 ? 0 : user_type_count + 1
+        })
+    };
+    // update users
+    users = Object.assign(users, updated_user);
+
+    console.log(users);
+    return images[type][user_type_count];
 }
 
-const handlePostback = (sender_psid, received_postback) => {
-  let response;
+function askTemplate(text){
+    return {
+        "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text": text,
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"Cats",
+                        "payload":"CAT_PICS"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"Dogs",
+                        "payload":"DOG_PICS"
+                    }
+                ]
+            }
+        }
+    }
+}
 
-  // Get the payload for the postback
-  let payload = received_postback.payload;
+function imageTemplate(type, sender_id){
+    return {
+        "attachment":{
+            "type":"image",
+            "payload":{
+                "url": getImage(type, sender_id),
+                "is_reusable":true
+            }
+        }
+    }
+}
 
-  if(payload === 'WELCOME'){
-    response = askTemplate('Très bien et vous ?');
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = askTemplate();
+    }
+
+    // Sends the response message
     callSendAPI(sender_psid, response);
-  }
 }
 
-const askTemplate = (text) => {
-  return {
-      "attachment":{
-          "type":"template",
-          "payload":{
-              "template_type":"button",
-              "text": text,
-              "buttons":[
-                  {
-                      "type":"postback",
-                      "title":"Je vais bien, merci",
-                      "payload":"YES"
-                  },
-                  {
-                      "type":"postback",
-                      "title":"Non, ça ne va pas",
-                      "payload":"NO"
-                  }
-              ]
-          }
-      }
-  }
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'CAT_PICS') {
+        response = imageTemplate('cats', sender_psid);
+        callSendAPI(sender_psid, response, function(){
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if (payload === 'DOG_PICS') {
+        response = imageTemplate('dogs', sender_psid);
+        callSendAPI(sender_psid, response, function(){
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if(payload === 'WELCOME'){
+        response = askTemplate('Are you a Cat or Dog Person?');
+        callSendAPI(sender_psid, response);
+    }
+    // Send the message to acknowledge the postback
 }
 
-
+// Sends response messages via the Send API
 function callSendAPI(sender_psid, response, cb = null) {
-  // Construct the message body
-  let request_body = {
-      "recipient": {
-          "id": sender_psid
-      },
-      "message": response
-  };
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
 
-  // Send the HTTP request to the Messenger Platform
-  request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
-  }, (err, res, body) => {
-      if (!err) {
-          if(cb){
-              cb();
-          }
-      } else {
-          console.error("Unable to send message:" + err);
-      }
-  });
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": config.get('facebook.page.access_token') },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            if(cb){
+                cb();
+            }
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
-
-// warming up the engines !! setta !! go !!!.
-app.listen(app.get('port'), function() {
-  const url = 'http://localhost:' + app.set('port');
-  console.log('Application running on port: ', app.get('port'));
-});
