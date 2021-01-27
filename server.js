dfdc7d4a30268d9b39e2407272fc7d4c1d51fa44
Abavisla <sivajana.bala@gmail.com>
2021-01-27 19:51:28 +0100

    1

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ function handlePostback(sender_psid, received_postback) {
 }
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response, resolve = null) {
+function callSendAPI(sender_psid, response) {
     // Construct the message body
 
     // Send the HTTP request to the Messenger Platform
@@ -172,8 +172,6 @@ function callSendAPI(sender_psid, response, resolve = null) {
       if (error) {
           console.log("Error sending message: " + response.error);
       reject(response.error);
-      } else {
-         resolve(body);
       }
  });
 }
