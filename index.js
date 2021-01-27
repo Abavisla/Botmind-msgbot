14a77977d528b15ae5aaa80e262d4150da31c4e6
Abavisla <sivajana.bala@gmail.com>
2021-01-27 03:21:43 +0100

    test

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const request = require('request');
 const app = express();
 
 
-app.set('port', (process.env.PORT || 8000));// 
+app.set('port', (process.env.PORT || 3000));// 
 app.use(morgan('dev')); // log every request to the console.
+
 
 
 app.use(bodyParser.urlencoded({ extended:false }));
@@ -25,17 +26,19 @@ app.get('/msgbot', function (req, res) {
 app.post('/msgbot', function(req, res) {
     //checking for page subscription.
     if (req.body.object === 'page'){
-       
        /* Iterate over each entry, there can be multiple entries 
 	   if callbacks are batched. */       
 	   req.body.entry.forEach(function(entry) {
        // Iterate over each messaging event
           entry.messaging.forEach(function(event) {
           console.log(event);
+          const sender = event.sender.id;
           if (event.postback){
              processPostback(event);
           } else if (event.message){
              processMessage(event);
+             sendTextMessage(sender, text + "!");
+
           }
       });
     });
@@ -43,6 +46,33 @@ app.post('/msgbot', function(req, res) {
    }
   });
 
+  const token = "EAAPTsUnoxzQBANSgosW7FhNPkEKlFsZA2VcSQd2ZCtlSdbyRxIa2Q4ytuKL0h7AHtZCoFs80TpladRIE2aOmwDrfaWGA3HGguj7MjWmzHXEPPtn2ZA96PidmWYZBNmcBzsn1ANnWZA82p317GFVRhGqQMNRMWM8BzSwAMRrqPG3QZDZD";
+  
+function sendTextMessage(sender, text) {
+   var messageData = {
+       text: text
+   };
+   request({
+       url: 'https://graph.facebook.com/v2.6/me/messages',
+       qs: {
+           access_token: token
+       },
+       method: 'POST',
+       json: {
+           recipient: {
+               id: sender
+           },
+           message: messageData,
+       }
+   }, function(error, response, body) {
+       if (error) {
+           console.log('Error:', error);
+       } else if (response.body.error) {
+           console.log('Error: ', response.body.error);
+       }
+   });
+}
+  
 app.listen(app.get('port'), function() {
   const url = 'http://localhost:' + app.set('port');
   console.log('Application running on port: ', app.get('port'));
