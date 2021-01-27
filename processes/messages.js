adf84acdc86db9fb19968e1888269a87480fc9ed
Abavisla <sivajana.bala@gmail.com>
2021-01-27 18:02:12 +0100

    message update

--- processes/messages.js
+++ processes/messages.js
@@ -8,10 +8,16 @@ module.exports = function processMessage(event) {
         const senderID = event.sender.id;
         console.log("Received message from senderId: " + senderID);
         console.log("Message is: " + JSON.stringify(message));
-        if (message.text) {
-            // now we will take the text recieved and send it to an food tracking API.
-            let text = message.text;
+        if (message.text === "Comment vas-tu ?") {
 
+            response = askTemplate('Très bien et vous ?');
+            callSendAPI(sender_psid, response);
+        }
+        else{
+
+            var messageData = {
+                text: message.text
+            };
             request({
                 url: 'https://graph.facebook.com/v2.6/me/messages',
                 qs: {
@@ -22,7 +28,7 @@ module.exports = function processMessage(event) {
                     recipient: {
                         id: sender
                     },
-                    message: text,
+                    message: messageData+"!!!!",
                 }
             }, function(error, response, body) {
                 if (error) {
@@ -32,4 +38,56 @@ module.exports = function processMessage(event) {
                 }
             });
         }
+    }
+
+
+    const askTemplate = (text) => {
+        return {
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text": text,
+                    "buttons":[
+                        {
+                            "type":"postback",
+                            "title":"Je vais bien, merci",
+                            "payload":"YES"
+                        },
+                        {
+                            "type":"postback",
+                            "title":"Non, ça ne va pas",
+                            "payload":"NO"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    
+    // Sends response messages via the Send API
+    const callSendAPI = (sender_psid, response, cb = null) => {
+        // Construct the message body
+        let request_body = {
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
+        };
+    
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": config.get('facebook.page.access_token') },
+            "method": "POST",
+            "json": request_body
+        }, (err, res, body) => {
+            if (!err) {
+                if(cb){
+                    cb();
+                }
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
     }
